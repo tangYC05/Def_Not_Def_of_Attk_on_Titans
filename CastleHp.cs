e7c0443ed08b61e9395bf7aee64f52e94c45956e
tangYC05 <35985945+tangYC05@users.noreply.github.com>
2018-01-31 13:08:20 +0800

    Add files via upload

---
+++ CastleHp.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CastleHp : MonoBehaviour {
+
+	public static int castleHp;
+	Text castleHealthText;	
+
+	void Awake () 
+	{
+		castleHealthText = GetComponent<Text> ();
+		castleHp = 200;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//Update the wall's health
+		castleHealthText.text = "Castle Health: " + castleHp;
+	}
+}
