1dd99b70fa2a3586c1c8df13f12814e506472dd3
tangYC05 <35985945+tangYC05@users.noreply.github.com>
2018-01-31 13:08:43 +0800

    Add files via upload

---
+++ DestroyBall.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyBall : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnCollisionEnter(Collision col)
+	{
+		//If collide with enemy, land and wall
+		if (col.gameObject.tag == "Enemy" || col.gameObject.tag == "Land" || col.gameObject.tag == "Walls") 
+		{
+			//Destroy itself
+			Destroy (gameObject);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
---
+++ DoubleBulletCount.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class DoubleBulletCount : MonoBehaviour {
+
+	public static int doubleBulletCount;
+	Text doubleBulletCountText;
+
+	void Awake()
+	{
+		doubleBulletCountText = GetComponent<Text> ();
+		doubleBulletCount = 20;
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		//Update the count left
+		doubleBulletCountText.text = "Double Shot left: " + doubleBulletCount;
+	}
+}
